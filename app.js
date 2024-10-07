13bd3c4d1da4411b8c965c56ed76d0abf701ffd9
OliverHaerle <136490738+OliverHaerle@users.noreply.github.com>
2024-10-07 18:16:10 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,190 @@
+clothes = [
+    {
+        name: 'T-Shirt',
+        price: 20,
+        material: 'cotton',
+        img: './img/clothes/white/t-shirt.png',
+        colors: ['white'],
+        color: 'white',
+    },
+    {
+        name: 'Button Down Shirt',
+        price: 25,
+        material: 'silk and cotton',
+        img: './img/clothes/white/Button Down Shirt.png',
+        colors: ['white', 'green'],
+        color: 'white',
+    },
+    {
+        name: 'Sweater',
+        price: 40,
+        material: 'cotton',
+        img: './img/clothes/white/sweater.png',
+        colors: ['white', 'green'],
+        color: 'white',
+    },
+    {
+        name: 'Skirt',
+        price: 15,
+        material: 'cotton',
+        img: './img/clothes/white/skirt.png',
+        colors: ['white', 'purple', 'red'],
+        color: 'white',
+    },
+    {
+        name: 'Dress',
+        price: 35,
+        material: 'cotton',
+        img: './img/clothes/white/dress.png',
+        colors: ['white', 'red', 'blue'],
+        color: 'white',
+    },
+    {
+        name: 'Hoodie',
+        price: 40,
+        material: 'wool',
+        img: './img/clothes/white/hoodie.png',
+        colors: ['white', 'purple'],
+        color: 'white',
+    }
+];
+cart = [];
+let price = 0;
+
+const clothesContainer = document.querySelector('.clothes-container');
+const cartContainer = document.querySelector('.cart');
+const priceContainer = document.querySelector('.price');
+
+window.addEventListener('DOMContentLoaded', function () {
+    displayClothingItems();
+    retrieveCart();
+});
+
+function displayClothingItems() {
+    let displayedItems = clothes.map((item, index) => {
+        const colorButtons = item.colors.map((color, colorIndex) => {
+            return `<button class="color-picker-btn ${color}" onclick="changeColor(${colorIndex}, ${index})"></button>`
+        }).join('');
+
+
+        return `
+    <article>
+        <div class="item-container">
+            <h4 class="name">${item.name}</h4>
+            <img id="item${index}" class="clothes-img" src="${item.img}" alt="${item.img}">
+            <div class="color-picker-container">${colorButtons}</div>
+            <div onclick="addToCart(${index})" class="add-to-cart-container">
+                <img class="add-to-cart" src="./img/shopping cart/add-to-cart.png">
+                <!-- <span>Added!</span> -->
+            </div>
+        </div>
+        <div class="price-and-material">
+            <span class="price">Price: $${item.price}</span>
+            <span class="material">Material: ${item.material}</span>
+            <div class="black-line"></div>
+        </div>
+    </article>`
+    });
+    displayedItems = displayedItems.join('');
+    clothesContainer.innerHTML = displayedItems;
+};
+
+function changeColor(colorIndex, index) {
+    const item = document.getElementById(`item${index}`);
+    const itemName = clothes[index].name;
+    const itemColor = clothes[index].colors[colorIndex];
+    item.src = `./img/clothes/${itemColor}/${itemName}.png`;
+
+    // register and save the current color in the clothes-array (needed for cart)
+    clothes[index].color = itemColor;
+};
+
+function addToCart(i) {
+    const item = { ...clothes[i] };
+
+    // Check if an item with the same color and name already exists in cart
+    const existingItem = cart.find(cartItem => cartItem.name === item.name && cartItem.color === item.color);
+
+    if (existingItem) {
+        existingItem.amount++
+    } else {
+        item.amount = 1;
+        cart.push(item)
+    };
+
+    displayCart();
+}
+
+function displayCart() {
+    let displayCart = cart.map((item, i) => {
+
+        if (item.amount == 0) {
+            return `
+            <article class="delete-and-undo">
+                <button class="undo-remove" onclick="undo(${i})">undo</button>
+                <button class="undo-remove" onclick="removeFromCart(${i})">remove</button>
+            </article>
+            `
+        } else {
+
+            return `
+        <div id="cart-item${i}" class="cart-item">
+            <b>${item.amount}x</b> ${item.name} - ${item.color}: $${item.amount * item.price}
+            <div id="amount-container${i}" class="change-amount">
+                <button onclick="manipulateAmount(-1, ${i})">
+                    <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill="#000000" fill-rule="evenodd" d="M18 10a1 1 0 01-1 1H3a1 1 0 110-2h14a1 1 0 011 1z"/></svg>                </button>
+                <button onclick="manipulateAmount(1, ${i})">
+                    <svg viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns"><g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"><g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-362.000000, -1037.000000)" fill="#000000"><path d="M390,1049 L382,1049 L382,1041 C382,1038.79 380.209,1037 378,1037 C375.791,1037 374,1038.79 374,1041 L374,1049 L366,1049 C363.791,1049 362,1050.79 362,1053 C362,1055.21 363.791,1057 366,1057 L374,1057 L374,1065 C374,1067.21 375.791,1069 378,1069 C380.209,1069 382,1067.21 382,1065 L382,1057 L390,1057 C392.209,1057 394,1055.21 394,1053 C394,1050.79 392.209,1049 390,1049" id="plus" sketch:type="MSShapeGroup"></path></g></g></svg>                
+                </button>
+            </div>
+        </div>
+            `
+        }
+    }).join('');
+    cartContainer.innerHTML = displayCart;
+    displayPrice();
+    saveCart();
+}
+
+function undo(i) {
+    cart[i].amount = 1;
+    displayCart();
+};
+
+function removeFromCart(i) {
+    cart.splice(i, 1);
+    displayCart();
+}
+
+function manipulateAmount(man, index) {
+    cart[index].amount += man;
+    displayCart();
+}
+
+function displayPrice() {
+    price = 0;
+    cart.forEach(itemInCart => {
+        price += itemInCart.amount * itemInCart.price;
+    });
+    if (cart.length > 0) {
+        priceContainer.innerHTML = '<b>Total:</b> $' + price;
+    } else {
+        priceContainer.innerHTML = ''
+    }
+};
+
+// save and load cart
+function saveCart() {
+    cartString = JSON.stringify(cart);
+    localStorage.setItem('cart', cartString);
+};
+
+function retrieveCart() {
+    const retrievedCartString = localStorage.getItem('cart');
+
+    if (retrievedCartString) {
+        const retrievedArray = JSON.parse(retrievedCartString);
+        cart = retrievedArray;
+        displayCart();
+    }
+}
